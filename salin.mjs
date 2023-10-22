cf6cc49b0f49e592de1d7f2fbbdf960ca13079ca
Lean <private@example.com>
2023-10-22 15:54:19 +1100

    ayusin ang mga bagay-bagay

--- salin.mjs
+++ salin.mjs
@@ -2,6 +2,7 @@ const tl = {
   pakilala: () => "Pansalin at panlikha ng kaligrapiyang baybayin",
   tagalogNaSalita: () => "Tagalog na salita",
   pagkakabasa: () => "Pagkakabasa",
+  lumikha: () => "·Lumikha·",
   diWastongLetra: (letra) => `'Di kayang gawin itong letra/simbolo: ${letra}. Maaaring gamitin lamang ay ang abakadang Pilipino.`
 };
 
@@ -9,10 +10,11 @@ const en = {
   pakilala: () => "Baybayin transliterator & calligraphy generator",
   tagalogNaSalita: () => "Tagalog word",
   pagkakabasa: () => "Interpretation",
+  lumikha: () => "·Generate·",
   diWastongLetra: (letra) => `Can't convert this letter/symbol: ${letra}. You may only use the Pilipino alphabet (abakada).`
 };
 
-export const salin = typeof window !== "undefined" && /\btl\b/.test(window.location.hash) ? tl : en;
+export const salin = typeof window !== "undefined" && /\btl\b/.test(window.location.search) ? tl : en;
 
 export function isalinAngPahina() {
   for (const elemento of document.querySelectorAll("[data-salin]")) {
